c98ee68c66b3a922963445fced092b7919068541
Duncan Murdock <me@duncanmurdock.name>
2014-02-12 04:07:52 -0600

    no

--- pelicanconf.py
+++ pelicanconf.py
@@ -37,19 +37,6 @@ PATH = 'content'
 PAGE_DIR = 'pages'
 THEME = 'pelican-bootstrap3'
 BOOTSTRAP_THEME = 'flatly'
-ARTICLE_URL = ('articles/{slug}/')
-ARTICLE_SAVE_AS = ('articles/{slug}/index.html')
-PAGE_URL = ('pages/{slug}/')
-PAGE_SAVE_AS = ('pages/{slug}/index.html')
-PAGE_LANG_SAVE_AS = False
-TAG_URL = ('tag/{slug}/')
-TAG_SAVE_AS = ('tag/{slug}/index.html')
-TAGS_URL = ('tags/')
-TAGS_SAVE_AS = None
-CATEGORY_URL = ('category/{slug}/')
-CATEGORY_SAVE_AS = ('category/{slug}/index.html')
-AUTHOR_SAVE_AS = False
-
 # Feed.
 FEED_DOMAIN = SITEURL
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
